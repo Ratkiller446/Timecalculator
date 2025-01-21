83ded202eefa630c75a12f3bcd0507b47a6f06c0
Janne <98688686+Ratkiller446@users.noreply.github.com>
2025-01-21 14:48:12 +0200

    Add files via upload

---
+++ timecalc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h>
+
+time_t calculatehours() {
+    time_t hours = 0;
+    printf("How many hours? ");
+    scanf("%ld", &hours);
+    return hours;
+}
+
+time_t calculateminutes() {
+    time_t minutes = 0;
+    printf("How many minutes? ");
+    scanf("%ld", &minutes);
+    return minutes;
+}
+
+
+int main() {
+    
+
+    time_t hours1 = 0, minutes1 = 0, hours2 = 0, minutes2 = 0;
+    time_t totalHours = 0, totalMinutes = 0;
+
+    printf("First time input:\n");
+    hours1 = calculatehours();
+    minutes1 = calculateminutes();
+
+    printf("Second time input:\n");
+    hours2 = calculatehours();
+    minutes2 = calculateminutes();
+
+    totalHours = hours1 + hours2;
+    totalMinutes = minutes1 + minutes2;
+
+    totalHours += totalMinutes / 60;  // Convert excess minutes to hours
+    totalMinutes = totalMinutes % 60;  // Keep minutes within 0-59
+    totalHours = totalHours % 24;  // Keep hours within 0-23
+
+    printf("Total time: %ld:%02ld\n", totalHours, totalMinutes);
+    return 0;
+}
+
